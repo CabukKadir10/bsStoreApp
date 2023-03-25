5313dd26a909f610ede1a19e1d41116938b533f5
Kadir Çabuk <109894656+CabukKadir10@users.noreply.github.com>
2023-03-25 14:23:07 +0300

    Update ErrorDetails.cs

--- Entities/ErrorModel/ErrorDetails.cs
+++ Entities/ErrorModel/ErrorDetails.cs
@@ -10,7 +10,7 @@ namespace Entities.ErrorModel
     public class ErrorDetails 
     {
         public int StatusCode { get; set; }
-        public string? Message { get; set; } // soru işareti null check yapmamızı sağlar
+        public string? Message { get; set; } 
 
         public override string ToString()
         {
