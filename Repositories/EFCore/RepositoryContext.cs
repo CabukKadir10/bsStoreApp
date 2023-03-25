f738013a650247bb1882ff2fa639f03f85204fac
Kadir Çabuk <109894656+CabukKadir10@users.noreply.github.com>
2023-03-25 14:28:40 +0300

    Update RepositoryContext.cs

--- Repositories/EFCore/RepositoryContext.cs
+++ Repositories/EFCore/RepositoryContext.cs
@@ -9,8 +9,8 @@ using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Repositories.EFCore // veri tabanı oluşturmak ve yönetmek için kullnaılır. IdentityDbContext ile kimlik doğrulaması işlevi kazandırır api'ye
-{//
+namespace Repositories.EFCore 
+{
     public class RepositoryContext : IdentityDbContext<User>
     {
         public RepositoryContext(DbContextOptions options) :
@@ -18,12 +18,12 @@ namespace Repositories.EFCore // veri tabanı oluşturmak ve yönetmek için kul
         {
 
         }
-        public DbSet<Book> Books { get; set; } // Book tablosuna erişmek için set etmemiz lazim
-        public DbSet<Category> Categories { get; set; }//Categories tablosuna erişmek için set yapmamiz lazım
+        public DbSet<Book> Books { get; set; } 
+        public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);// veri tabanı modellemesi için kullanılır. bu yüzden bookConfig örnek alır
+            base.OnModelCreating(modelBuilder);
            // modelBuilder.ApplyConfiguration(new BookConfig());
            // modelBuilder.ApplyConfiguration(new RoleConfiguration());
            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
