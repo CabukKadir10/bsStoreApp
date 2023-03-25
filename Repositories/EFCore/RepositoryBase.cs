2c570494b701ac59ecd9d99a961ff3f685cc02c5
Kadir Çabuk <109894656+CabukKadir10@users.noreply.github.com>
2023-03-25 14:28:03 +0300

    Update RepositoryBase.cs

--- Repositories/EFCore/RepositoryBase.cs
+++ Repositories/EFCore/RepositoryBase.cs
@@ -19,13 +19,13 @@ namespace Repositories.EFCore
             _context = context;
         }
 
-        public void Create(T entity) => _context.Set<T>().Add(entity); // alınan özellikle set aracılıgıyla veri tabanına ekler
+        public void Create(T entity) => _context.Set<T>().Add(entity); 
 
-        public void Delete(T entity) => _context.Set<T>().Remove(entity); // alınan özellikleri siler ve veri tabanına kaydeder
+        public void Delete(T entity) => _context.Set<T>().Remove(entity); 
 
         public IQueryable<T> FindAll(bool trackChanges) =>
             !trackChanges ?
-            _context.Set<T>().AsNoTracking() :// asnotracking varlık sorgulaması yaparken performansı artırmak için kullanılır. sonuç olarak tüm veri tabanına erişim sağlanır.
+            _context.Set<T>().AsNoTracking() :
             _context.Set<T>();
 
         public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
@@ -34,7 +34,7 @@ namespace Repositories.EFCore
             _context.Set<T>().Where(expression).AsNoTracking() :
             _context.Set<T>().Where(expression);
 
-        public void Update(T entity) => _context.Set<T>().Update(entity); // güncelleme işlemini yapar veri tabanında
+        public void Update(T entity) => _context.Set<T>().Update(entity); 
 
     }
 }
