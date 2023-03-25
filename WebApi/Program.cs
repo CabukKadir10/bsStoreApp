ca8e87a3f163b80bfe359ebb896df7f2772bd9fc
Kadir Çabuk <109894656+CabukKadir10@users.noreply.github.com>
2023-03-25 14:15:14 +0300

    Update Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -7,16 +7,16 @@ using WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Nlog için yapýlandýrma ayarlarýný nlog.config dosyasýna yükler.
+
 LogManager.LoadConfiguration(String.Concat(Directory.GetCurrentDirectory(),"/nlog.config"));
 
 builder.Services.AddControllers(config =>
 {
-    config.RespectBrowserAcceptHeader = true; // tarayýcýdan gelen accept header deðerini kullanarak yanýtýn formatýný belirliyor.
-    config.ReturnHttpNotAcceptable = true; // yanýt formatý xml, csv vb olabilir. eger olmazsa 406 hatasýný döner
+    config.RespectBrowserAcceptHeader = true; 
+    config.ReturnHttpNotAcceptable = true; 
     config.CacheProfiles.Add("5mins", new CacheProfile() { Duration = 300 });
 })
-.AddXmlDataContractSerializerFormatters() // xml serileþtirme desteði ekliyoruz
+.AddXmlDataContractSerializerFormatters()
 .AddCustomCsvFormatter()
 .AddApplicationPart(typeof(Presentation.AssemblyReference).Assembly)
 .AddNewtonsoftJson(opt => 
@@ -26,20 +26,20 @@ builder.Services.AddControllers(config =>
   
 
 builder.Services.Configure<ApiBehaviorOptions>(options =>
-{ // apý davranýþ ayarlarýný yapýlandýrýr ve model durumunu yanlýþ filtrelemeden ayarlar.
+{ 
     options.SuppressModelStateInvalidFilter = true;
 });
 
 
-builder.Services.AddEndpointsApiExplorer(); // bu iki satýr swagger arayüzü için gerekli olan bileþenleri ekler
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.ConfigureSwagger();
 
-builder.Services.ConfigureSqlContext(builder.Configuration); // veri tabaný iþlemleri için kullanýlýr
+builder.Services.ConfigureSqlContext(builder.Configuration); 
 builder.Services.ConfigureRepositoryManager();
 builder.Services.ConfigureServiceManager();
 builder.Services.ConfigureLoggerService();
 builder.Services.AddAutoMapper(typeof(Program));
-builder.Services.ConfigureActionFilters(); // web uygulamasýndaki farkli servislerin yapýlandýrýldýðý yer
+builder.Services.ConfigureActionFilters(); 
 builder.Services.ConfigureCors();
 builder.Services.ConfigureDataShaper();
 builder.Services.AddCustomMediaTypes();
@@ -60,19 +60,19 @@ var app = builder.Build();
 var logger = app.Services.GetRequiredService<ILoggerService>();
 app.ConfigureExceptionHandler(logger);
 
-if (app.Environment.IsDevelopment())//eger development aþamasýnda ise swaggera geçiþ yapar
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(s =>
     {
-        s.SwaggerEndpoint("/swagger/v1/swagger.json", "BTK Akademi v1");
-        s.SwaggerEndpoint("/swagger/v2/swagger.json", "BTK Akademi v2");
+        s.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        s.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
     });
 }
 
-if (app.Environment.IsProduction())//eðer prodyct aþamasýnda ise Hsts'yi çalýþtýrýr.
+if (app.Environment.IsProduction())
 {
-    app.UseHsts(); // uygulamanýn çalýþmaya hazýr gelmesini saðlayan kod parçasýdýr.
+    app.UseHsts(); 
 }
 
 app.UseHttpsRedirection();
@@ -88,15 +88,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-
-//katmanlý mimarý,
-//repository pattern:  veri tabaný metotlarýnýn yazýldýgý katmandýr. 
-//generic repository: Crud iþlemlerinin yönetildiði katmandýr.
-// busines nedir temelde ne iþ yapýlýyor. 
-// dp injection
-// sevice register iþlemleri(auto fake)
-//fluent api-entity frame work core
-//extentions method
-//neden intefcaea kullanýlýr
-//ýoc conteiner     
